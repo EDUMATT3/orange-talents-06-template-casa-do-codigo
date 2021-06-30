ac2083f545d80552a1f24a2bdb9a7b527eba4c6b
eduardo-zup <eduardo.bento@zup.com.br>
2021-06-30 15:43:04 -0300

    add autores no banco

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into autor(id, nome, email, descricao, created_at) values
+(null, 'nome 1', 'nome1@email.com', 'anydesc', now()),
+(null, 'nome 2', 'nome2@email.com', 'anydesc', now()),
+(null, 'nome 3', 'nome3@email.com', 'anydesc', now());
