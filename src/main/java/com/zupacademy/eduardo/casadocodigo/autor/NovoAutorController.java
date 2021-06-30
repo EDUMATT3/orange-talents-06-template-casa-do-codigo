5453996fecf87a5bc56501f54481e2ae7d9ddeed
eduardo-zup <eduardo.bento@zup.com.br>
2021-06-30 10:06:05 -0300

    add enpoint and form

--- src/main/java/com/zupacademy/eduardo/casadocodigo/autor/NovoAutorController.java
+++ src/main/java/com/zupacademy/eduardo/casadocodigo/autor/NovoAutorController.java
@@ -1,2 +1,20 @@
-package com.zupacademy.eduardo.casadocodigo.autor;public class NovoAutorController {
+package com.zupacademy.eduardo.casadocodigo.autor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("autores")
+public class NovoAutorController {
+
+    @PostMapping
+    public ResponseEntity<?> novoAutor(@RequestBody @Valid NovoAutorRequest novoAutorRequest){
+        return ResponseEntity.ok().body(novoAutorRequest.toString());
+    }
+
 }
