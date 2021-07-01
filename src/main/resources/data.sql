59441a602ad59d7128add4df08bbbf95f7152f31
eduardo-zup <eduardo.bento@zup.com.br>
2021-07-01 11:11:57 -0300

    add categorias no banco

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,8 @@ insert into autor(id, nome, email, descricao, created_at) values
 (null, 'nome 1', 'nome1@email.com', 'anydesc', now()),
 (null, 'nome 2', 'nome2@email.com', 'anydesc', now()),
 (null, 'nome 3', 'nome3@email.com', 'anydesc', now());
+
+insert into categoria(id, nome) values
+(null, 'categoria 1'),
+(null, 'categoria 2'),
+(null, 'categoria 3');
